24f27731776c6171b48850270789f2277ce14659
Devanshi Mishra <devanshi.mishra.06@gmail.com>
2022-03-02 22:13:20 +0530

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) 
+{
+    var maxChar, maxCharCount = 0, countChar = new Array(256).fill(0); ;
+    for(var i = 0; i < str.length; i++)
+    {
+        countChar[str.charCodeAt(i)]++;
+    }
+    for(var i = 0; i < str.length; i++)
+    {
+        if (countChar[str.charCodeAt(i)] > maxCharCount)
+        {
+            maxCharCount = countChar[str.charCodeAt(i)];
+            maxChar = str[i];
+        }
+    }
+    return maxChar;
+}
 
 module.exports = maxChar;

513736e9d3d80d8894eac14096f99267c55cba7f
Devanshi Mishra <devanshi.mishra.06@gmail.com>
2022-03-02 21:23:00 +0530

    Reverse an int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,23 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) 
+{
+    var s = 0, isNegative = false, r;
+    if (n < 0)
+    {
+        isNegative = true;
+        n = n * -1;
+    }
+    
+    while(Math.floor(n/10) > 0)
+    {
+        r = n % 10;
+        s = s * 10 + r;
+        n = Math.floor(n/10);
+    }
+    s = s *  10 + n; 
+    return isNegative === true? -1 * s : s;
+}
 
 module.exports = reverseInt;
